a61f15d986e5c771cf00c9a878fd8bb6a3295125
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-06-23 05:49:35 +0300

    moved the cmd-init to cli.py

--- cli.py
+++ cli.py
@@ -1,4 +1,14 @@
 import argparse
+from libwyag import *
+
+def cmd_init(args):
+    """
+    The Repo init logic
+    1-receive the command as a path.command in the main matcher
+    2 this function "cmd-init(args)" gets called and calles repo_creat(args.path)
+
+    """
+    repo_create(args.path)
 
 
 def parse_args(argv):
@@ -8,14 +18,29 @@ def parse_args(argv):
     argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
     argsubparsers.required = True
 
+    # init parser
     init_parser = argsubparsers.add_parser("init", help="init")
-
     init_parser.add_argument(
         "path",
         metavar="directory",
         nargs="?",
         default=".",
         help="Where to create the repository.",
+    )
+
+    # cat-file parser
+    cat_file_parser = argparser.add_subparsers(
+        "cat-file", help="Provide content of repository objects"
+    )
+
+    cat_file_parser.add_argument(
+        "type",
+        meravar="type",
+        choices=["blob", "commit", "tag", "tree"],
+        help="Specify the type",
+    )
+    cat_file_parser.add_argument(
+        "object", metavar="object", help="The object to Display"
     )
 
     return argparser.parse_args(argv)
