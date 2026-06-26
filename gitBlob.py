14d50ec11a1582a5d95add017eda2568d1b2b0ef
Mahmoud-Othman <mahmoudmohsen12344@gmail.com>
2026-06-26 09:55:55 +0300

    aded the gitBlob module and moved the gitBlob logic to it

---
+++ gitBlob.py
@@ -0,0 +1,13 @@
+from gitObjects import GitObject
+
+
+class GitBlob(GitObject):
+    """git blob is a git object represents a File in the git slang"""
+
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
